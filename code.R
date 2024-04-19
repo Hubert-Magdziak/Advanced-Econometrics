49fdaaf3af210914e90afc5abf89653e4ee4f60c
Hubert-Magdziak <hubert_magdziak@o2.pl>
2024-04-19 10:17:23 +0200

    Decoding nominal variables into dummies

--- code.R
+++ code.R
@@ -40,12 +40,23 @@ data <- data %>%
   filter(ST.slope > 0)
 
 barplot(table(data$resting.ecg))
-# Convert variables to appropriate type
+# Convert nominal variables into dummies
 
-data$chest.pain.type <- as.factor(data$chest.pain.type)
-data$ST.slope <- as.factor(data$ST.slope)
-data$resting.ecg <- as.factor(data$resting.ecg)
-str(data)
+# Variable "chest.pain.type" - base level "typical angina"
+data$chest.pain.type.atypical.angina <- ifelse(data$chest.pain.type == 2, 1, 0)
+data$chest.pain.type.non.anginal <- ifelse(data$chest.pain.type == 3, 1, 0)
+data$chest.pain.type.asymptomatic <- ifelse(data$chest.pain.type == 4, 1, 0)
+
+# Variable "resting.ecg" - base level "normal"
+data$resting.ecg.1 <- ifelse(data$resting.ecg == 1, 1, 0)
+data$resting.ecg.2 <- ifelse(data$resting.ecg == 2, 1, 0)
+
+# Variable "St.slope" - base level "upsloping"
+data$ST.slope.flat <- ifelse(data$ST.slope == 2, 1, 0)
+data$ST.slope.downsloping <- ifelse(data$ST.slope == 3, 1, 0)
+
+# Finally prepared dataset
+data <- data[, -c(3, 7, 11)]
 
 # Model
 
@@ -55,8 +66,7 @@ summary(logit_0)
 
 # Linktest
 source("linktest.R")
-linktest_logit_0 = linktest(logit_0)
-summary(linktest_logit_0)
+summary(linktest(logit_0))
 
 # RESULT: The specification of our model is correct
 
@@ -78,5 +88,12 @@ hltest(logit_0)
 
 # p-value = 0.28% > 5% (significance level alpha), we fail to reject H0
 
+# Genaral to specific approach (backward selection)
 
-
+# Check if variable resting.ecg is statistically significant
+# H0: resting.ecg = 0
+logit_1 <- glm(target ~ age + sex + chest.pain.type + resting.bp.s, + cholesterol
+               + fasting.blood.sugar + max.heart.rate + exercise.angina
+               + oldpeak + ST.slope + I(age^2) + age:max.heart.rate, 
+               data = data, family = binomial('logit'))
+summary(logit_1)
